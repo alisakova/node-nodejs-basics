14a3ff362d019f3980d8afd8e63d135f12eb7ac6
alisakova <a.lisakova@morbo.ru>
2022-12-01 21:28:05 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,37 @@
+import { access, writeFile } from "fs/promises";
+import { join } from "path";
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const FILE_NAME = "fresh.txt";
+const CONTENT = "I am fresh and young";
+const ERROR_MESSAGE = "FS operation failed";
+const FILE_DIRECTORY = "files";
+const FILE_PATH = join(__dirname, FILE_DIRECTORY, FILE_NAME);
+
+const isExist = async (filePath) => {
+    try {
+        await access(filePath);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const isFileExist = await isExist(FILE_PATH);
+
+        if (isFileExist) {
+            throw new Error(ERROR_MESSAGE);
+        }
+
+        await writeFile(join(__dirname, FILE_DIRECTORY, FILE_NAME), CONTENT);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await create();
