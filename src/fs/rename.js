90f58363fd0494f30cbca8869930f9cbf08b3d0b
alisakova <a.lisakova@morbo.ru>
2022-12-01 21:29:42 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,39 @@
+import { rename as fsRename, access } from "fs/promises";
+import path from "path";
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const OLD_FILE_NAME = "wrongFilename.txt";
+const NEW_FILE_NAME = "properFilename.md";
+const ERROR_MESSAGE = "FS operation failed";
+const FILE_DIRECTORY = "files";
+const OLD_FILE_PATH = path.join(__dirname, FILE_DIRECTORY, OLD_FILE_NAME);
+const NEW_FILE_PATH = path.join(__dirname, FILE_DIRECTORY, NEW_FILE_NAME);
+
+const isExist = async (filePath) => {
+    try {
+        await access(filePath);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const isNewFileExist = await isExist(NEW_FILE_PATH);
+        const isOldFileExist = await isExist(OLD_FILE_PATH);
+
+        if (isNewFileExist || !isOldFileExist) {
+            throw new Error(ERROR_MESSAGE)
+        }
+
+        await fsRename(OLD_FILE_PATH, NEW_FILE_PATH);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await rename();
