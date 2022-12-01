3493f40ad5a536277190bc9cd5da57aebed70c84
alisakova <a.lisakova@morbo.ru>
2022-12-01 21:43:09 +0300

    refactor: add isExist check in list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,6 +1,7 @@
 import { readdir } from "fs/promises";
 import { join } from "path";
 import url from "url";
+import { isExist } from "./isExist.js";
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -10,13 +11,19 @@ const DIRECTORY_PATH = join(__dirname, FILE_DIRECTORY);
 
 const list = async () => {
     try {
+        const isDirectoryExist = isExist(DIRECTORY_PATH);
+
+        if (!isDirectoryExist) {
+            throw new Error(ERROR_MESSAGE);
+        }
+
         const files = await readdir(DIRECTORY_PATH);
 
         for (const file of files) {
             console.log(file);
         }
     } catch (error) {
-        console.error(ERROR_MESSAGE);
+        console.error(error.message);
     }
 };
 
