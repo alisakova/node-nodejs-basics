2159de2a70b30bf10cf2fa858fab88ffb37dec3c
alisakova <a.lisakova@morbo.ru>
2022-12-01 22:31:18 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    const [first, second, ...rest] = process.argv;
+
+    const result = rest.reduce((acc, value, index) => {
+        if (value.includes("--")) {
+            acc.push(value, " is ");
+
+            return acc;
+        }
+
+        acc.push(value, index === rest.length - 1 ? "" : ", ");
+
+        return acc;
+    }, []);
+
+    console.log(result.join(""));
 };
 
 parseArgs();
