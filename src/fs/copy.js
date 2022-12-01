55816acee08d7f2c5663dea68e51f08d634781ca
alisakova <a.lisakova@morbo.ru>
2022-12-01 21:29:04 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { readdir, mkdir, copyFile } from "fs/promises";
+import { join } from "path";
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const NEW_FILE_DIRECTORY = "files_copy";
+const ERROR_MESSAGE = "FS operation failed";
+const FILE_DIRECTORY = "files";
+const DIRECTORY_PATH = join(__dirname, FILE_DIRECTORY);
+const NEW_DIRECTORY_PATH = join(__dirname, NEW_FILE_DIRECTORY)
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(DIRECTORY_PATH);
+        await mkdir(NEW_DIRECTORY_PATH);
+
+        for (const file of files) {
+            await copyFile(join(DIRECTORY_PATH, file), join(NEW_DIRECTORY_PATH, file));
+        }
+    } catch (error) {
+        console.error(ERROR_MESSAGE);
+    }
 };
 
-copy();
+await copy();
