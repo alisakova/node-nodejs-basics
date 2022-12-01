4c65a14d56eff863f6633841689606aa8a5a95f4
alisakova <a.lisakova@morbo.ru>
2022-12-01 22:14:07 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+import { argv } from "node:process";
+
+const ENV_PREFIX = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+    const envs = Object.entries(process.env).reduce((result, [key, value]) => {
+        if (key.includes(ENV_PREFIX)) {
+            result.push(`${key}=${value}`);
+
+            return result;
+        }
+
+        return result;
+    }, []);
+
+    console.log(envs.join("; "));
 };
 
 parseEnv();
