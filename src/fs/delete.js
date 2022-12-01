c1121bfaed40c27ec07df2e8a99f1e89e3b9f8e6
alisakova <a.lisakova@morbo.ru>
2022-12-01 21:30:14 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,36 @@
+import { rm, access } from "fs/promises";
+import path from "path";
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const FILE_TO_DELETE = "fileToRemove.txt";
+const ERROR_MESSAGE = "FS operation failed";
+const FILE_DIRECTORY = "files";
+const FILE_PATH = path.join(__dirname, FILE_DIRECTORY, FILE_TO_DELETE);
+
+const isExist = async (filePath) => {
+    try {
+        await access(filePath);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const isFileExist = await isExist(FILE_PATH);
+
+        if (!isFileExist) {
+            throw new Error(ERROR_MESSAGE);
+        }
+
+        await rm(FILE_PATH);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await remove();
