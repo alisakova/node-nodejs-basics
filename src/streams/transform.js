9a44be3fb2c337e74a233f91cfba56e335caa591
alisakova <a.lisakova@morbo.ru>
2022-12-02 20:31:49 +0300

    feat: implement streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+
+const { stdin, stdout } = process;
+
 const transform = async () => {
-    // Write your code here 
+    const transform = new Transform({
+        transform(chunk, encoding, callback) {
+            const stringChunk = chunk.toString().trim();
+            const reversedString = stringChunk.split("").reverse().join("");
+
+            callback(null, reversedString + "\n");
+        }
+    });
+
+    try {
+        await pipeline(stdin, transform, stdout);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await transform();
