d6d27382a69a56d89b02b803c089b6cc5b8eab87
alisakova <a.lisakova@morbo.ru>
2022-12-04 18:54:44 +0300

    feat: implement wt/main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import { join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const CPUS_NUMBER = os.cpus().length;
+const FILE_NAME = "worker.js";
+const PATH_TO_FILE = join(__dirname, FILE_NAME);
+const START_NUMBER = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+    const threads = new Set();
+
+    for (let i = 0; i < CPUS_NUMBER; i++) {
+        threads.add(new Promise((resolve, reject) => {
+            const worker = new Worker(PATH_TO_FILE, { workerData: START_NUMBER + i });
+            worker.on("message", resolve);
+            worker.on("error", reject);
+        }));
+    }
+
+    Promise.allSettled(threads)
+        .then((responses => responses.map(response => ({
+        status: response.status === "fulfilled" ? "resolved" : "error",
+        data: response.value || null,
+    }))))
+        .then(result => console.log(result));
 };
 
 await performCalculations();
