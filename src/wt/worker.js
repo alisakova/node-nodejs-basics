27f3fbbac98d4502d9161eda9e73c9ef4030150a
alisakova <a.lisakova@morbo.ru>
2022-12-04 17:05:51 +0300

    feat: implement wt/worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    if (isMainThread) {
+        const worker = new Worker(__filename, { workerData: 7 });
+        worker.on("message", (message) => { console.log(message); });
+    } else {
+        const result = nthFibonacci(workerData);
+        parentPort.postMessage(result);
+    }
 };
 
 sendResult();
