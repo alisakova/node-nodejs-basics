50e21d94c7fb5e6f996feae7401e33d16b8e9d7f
alisakova <a.lisakova@morbo.ru>
2022-12-05 22:17:25 +0300

    feat: implement cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from "child_process";
+import { fileURLToPath } from "url";
+import { join } from "path";
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const FILE_PATH = join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    fork(FILE_PATH, args);
 };
 
 spawnChildProcess();
